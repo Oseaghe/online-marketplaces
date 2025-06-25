40a6c68206f4e7366d80245df40db33b9d4af489
Oseaghe <oseagheebhomielen@gmail.com>
2025-06-25 09:14:23 +0100

    init commit

--- src/main/java/com/oseaghe/store/repositories/UserRepository.java
+++ src/main/java/com/oseaghe/store/repositories/UserRepository.java
@@ -1,8 +1,15 @@
 package com.oseaghe.store.repositories;
 
 import com.oseaghe.store.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
